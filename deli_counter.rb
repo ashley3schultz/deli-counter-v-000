0d93158d3096958c29dd1c621fbf2435b798fbc5
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-19 03:02:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,12 @@
 katz_deli = []
 
 def line(katz_deli)
-  num = katz_deli + 1
   line = ["The line is currently: "]
-  if katz_deli.size == 0 
+  if katz_deli.size == 0
     puts "The line is currently empty."
   else
     katz_deli.each {|place|
-    line << "#{num}. #{name} "}
+    line << "#{katz_deli[palce] + 1}. #{name} "}
     line
   end
 end
